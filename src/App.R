1eddd178e25c30fe5f1005e93f77a4a09de3640e
Chen Lin <chen.lin.0404@gmail.com>
2023-02-18 00:43:40 -0800

    update app file with server updated

--- src/App.R
+++ src/App.R
@@ -42,3 +42,66 @@ adv_stat <- url %>%
 
 # Merge stats tables
 total_stats <- merge(ttl_stat, adv_stat, by=c("Season","Age", "Tm", "Lg", "Pos", "G", "MP"))
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "journal"),
+  titlePanel(title=div(img(src="nba_logo.png", height='50px'), "NBA Player Stats Application Dashboard")),
+  fluidRow(column(width = 5,
+                  h4("Search by player name :"),
+                  sidebarPanel(
+                    width = 8,
+                    textInput("search", "", placeholder = "Search by player full name (first_name last_name)")),
+                  fluidRow(column(width = 3, align = "center", img(src=image_url, width=100)),
+                           column(width = 8, align = "center",
+                                  fluidRow(player),
+                                  fluidRow('Position:', player),
+                                  fluidRow('Age:', player),
+                                  fluidRow('Experience:', player))),
+                  h4("Filters :"),
+                  sidebarLayout(
+                    sidebarPanel(
+                      sliderInput(
+                        inputId = "careeryearslider",
+                        label = "Career Year",
+                        min = 1990,
+                        max = 2010,
+                        value = 1995
+                      )
+                    ),
+                    mainPanel(plotOutput(outputId = "distplot"))
+                  ),
+                  fluidRow(column(width = 2, "Team(s)")),
+                  fluidRow(column(width = 6, checkboxInput('change_sign','Team A'))),
+                  fluidRow(column(width = 6, checkboxInput('change_sign','Team B'))),
+                  fluidRow(column(width = 6, checkboxInput('change_sign','Team C'))),
+                  fluidRow(column(width = 6, checkboxInput('change_sign','Team D')))),
+           column(
+             width = 6,
+             h4("Points per game and shooting accuracy :"),
+             fluidRow(column(width = 8, align = "center", img(src="PTS.png", height='200px'))),
+             h4("Game played in each season :"),
+             fluidRow(column(width = 8, align = "center", img(src="GamePlayed.png", height='200px'))),
+             h4("Player Radar Chart :"),
+             fluidRow(column(width = 8, align = "center", img(src="PlayerRadar.png", height='250px'))),
+           ))
+)
+
+server <- function(input, output, session) {
+  
+  thematic::thematic_shiny()
+  output$distplot <- renderPlot({
+    # generate bins
+    x <- total_stats$PTS
+    bins <- seq(min(x), max(x), length.out = input$careeryearslider + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x,
+         breaks = bins, col = "darkgray", border = "white",
+         xlab = "Career year/season",
+         main = "Histogram of Points per game by season"
+    )
+  })
+  
+}
+
+
