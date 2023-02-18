b95b892b3c5a2e0ad3fc96804a93f74bdc8dd96a
Chen Lin <chen.lin.0404@gmail.com>
2023-02-18 00:41:28 -0800

    update app file with player name for api

--- src/App.R
+++ src/App.R
@@ -17,3 +17,14 @@ library(jpeg)
 
 # Using real time api to load nba player stats data
 
+# Add player name and player slug
+player <- "Jason Kidd"
+player_name_split <- tolower(strsplit(player, " ")[[1]])
+slug_ln <- substr(player_name_split[2], start = 1, stop = 5)
+slug_fn <- substr(player_name_split[1], start = 1, stop = 2)
+slug <- paste0(slug_ln, slug_fn, '01')#"bryanko01" 
+
+# define player page URL and player image URL
+url <- paste0("https://www.basketball-reference.com/players/",substr(slug,1,1),"/",slug,".html")
+image_url <- paste0("https://www.basketball-reference.com/req/202106291/images/players/",slug,".jpg")
+
