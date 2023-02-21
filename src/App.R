58197726a932f4b05a4851f38cc055dbf5e82810
Chen Lin <chen.lin.0404@gmail.com>
2023-02-21 14:03:51 -0800

    update teams mapping function

--- src/App.R
+++ src/App.R
@@ -29,41 +29,114 @@ position_map <- function(pos) {
 }
 
 nba_teams_map <- function(team_code) {
-  teams <- list(ATL = "Atlanta Hawks",
-                BKN = "Brooklyn Nets",
-                BOS = "Boston Celtics",
-                CHA = "Charlotte Hornets",
-                CHI = "Chicago Bulls",
-                CLE = "Cleveland Cavaliers",
-                DAL = "Dallas Mavericks",
-                DEN = "Denver Nuggets",
-                DET = "Detroit Pistons",
-                GSW = "Golden State Warriors",
-                HOU = "Houston Rockets",
-                IND = "Indiana Pacers",
-                LAC = "Los Angeles Clippers",
-                LAL = "Los Angeles Lakers",
-                MEM = "Memphis Grizzlies",
-                MIA = "Miami Heat",
-                MIL = "Milwaukee Bucks",
-                MIN = "Minnesota Timberwolves",
-                NOP = "New Orleans Pelicans",
-                NJN = "Brooklyn Nets",
-                NYK = "New York Knicks",
-                OKC = "Oklahoma City Thunder",
-                ORL = "Orlando Magic",
-                PHI = "Philadelphia 76ers",
-                PHO = "Phoenix Suns",
-                PHX = "Phoenix Suns",
-                POR = "Portland Trail Blazers",
-                SAC = "Sacramento Kings",
-                SAS = "San Antonio Spurs",
-                TOR = "Toronto Raptors",
-                TOT = "Two Other Teams",
-                UTA = "Utah Jazz",
-                WAS = "Washington Wizards")
+  teams <- list(ATL = "Atlanta Hawks (ATL)",
+                BKN = "Brooklyn Nets (BKN)",
+                BOS = "Boston Celtics (BOS)",
+                CHA = "Charlotte Hornets (CHA)",
+                CHI = "Chicago Bulls (CHI)",
+                CLE = "Cleveland Cavaliers (CLE)",
+                DAL = "Dallas Mavericks (DAL)",
+                DEN = "Denver Nuggets (DEN)",
+                DET = "Detroit Pistons (DET)",
+                GSW = "Golden State Warriors (GSW)",
+                HOU = "Houston Rockets (HOU)",
+                IND = "Indiana Pacers (IND)",
+                LAC = "Los Angeles Clippers (LAC)",
+                LAL = "Los Angeles Lakers (LAL)",
+                MEM = "Memphis Grizzlies (MEM)",
+                MIA = "Miami Heat (MIA)",
+                MIL = "Milwaukee Bucks (MIL)",
+                MIN = "Minnesota Timberwolves (MIN)",
+                NOP = "New Orleans Pelicans (NOP)",
+                NJN = "Brooklyn Nets (NJN)",
+                NYK = "New York Knicks (NYK)",
+                OKC = "Oklahoma City Thunder (OKC)",
+                ORL = "Orlando Magic (ORL)",
+                PHI = "Philadelphia 76ers (PHI)",
+                PHO = "Phoenix Suns (PHO)",
+                PHX = "Phoenix Suns (PHX)",
+                POR = "Portland Trail Blazers (POR)",
+                SAC = "Sacramento Kings (SAC)",
+                SAS = "San Antonio Spurs (SAS)",
+                TOR = "Toronto Raptors (TOR)",
+                TOT = "Two Other Teams (TOT)",
+                UTA = "Utah Jazz (UTA)",
+                WAS = "Washington Wizards (WAS)")
   return(teams[[team_code]])
 }
+
+# Add player name and player slug
+player <- "Jason Kidd"
+player_name_split <- tolower(strsplit(player, " ")[[1]])
+slug_ln <- substr(player_name_split[2], start = 1, stop = 5)
+slug_fn <- substr(player_name_split[1], start = 1, stop = 2)
+slug <- paste0(slug_ln, slug_fn, '01')#"bryanko01" 
+
+# define player page URL and player image URL
+url <- paste0("https://www.basketball-reference.com/players/",substr(slug,1,1),"/",slug,".html")
+image_url <- paste0("https://www.basketball-reference.com/req/202106291/images/players/",slug,".jpg")
+
+# Read total stats
+ttl_stat <- url %>%
+  read_html %>%
+  html_node("#totals") %>% 
+  html_table()
+
+# Read advanced stats
+adv_stat <- url %>%
+  read_html %>%
+  html_node("#advanced") %>% 
+  html_table()
+
+# Merge stats tables
+total_stats <- merge(ttl_stat, adv_stat, by=c("Season","Age", "Tm", "Lg", "Pos", "G", "MP"))
+
+# Prepare player stats table
+player_stats <- total_stats |> select(c('Season', 'Age', 'Tm', 'Pos', 'G', 'FG%', 'TRB', 'AST', 'STL', 'BLK', 'PTS'))
+
+player_stats <- player_stats |>
+  mutate('TRB per game' = round(player_stats$TRB / player_stats$G, 2),
+         'AST per game' = round(player_stats$AST / player_stats$G, 2),
+         'STL per game' = round(player_stats$STL / player_stats$G, 2),
+         'BLK per game' = round(player_stats$BLK / player_stats$G, 2),
+         'PTS per game' = round(player_stats$PTS / player_stats$G, 2))
+
+# remove missing values
+player_exp_no_na <- player_stats |> filter(!is.na(player_stats$Age))
+
+# Get PLayer Experience
+player_exp <- length(unique(player_exp_no_na$Age))
+
+# Get Player career year range
+player_first_season <- min(unique(player_exp_no_na$Season))
+player_last_season <- max(unique(player_exp_no_na$Season))
+
+# Get PLayer Age
+player_age <- max(player_exp_no_na$Age, na.rm = TRUE)
+
+# Get PLayer Positions
+player_position_ls <- unique(player_exp_no_na$Pos)
+
+player_positions <- ''
+
+for (i in seq_along(player_position_ls)) {
+  player_positions <- paste0(player_positions, 
+                             position_map(player_position_ls[i]), 
+                             collapse = "")
+  if (i != length(player_position_ls)) {
+    player_positions <- paste0(player_positions, ' & ', collapse = "")
+  }
+}
+
+# Get PLayer teams
+player_team_ls <- unique(player_exp_no_na$Tm)
+
+player_teams <- c()
+
+for (team in player_team_ls) {
+  player_teams <- c(player_teams, nba_teams_map(team))
+}
+
 
 # ---- UI----
 ui <- fluidPage(
@@ -87,12 +160,25 @@ ui <- fluidPage(
                 sliderInput(
                   inputId = "careeryearslider",
                   label = "Career Year",
-                  min = as.integer(substr(player_first_season, start = 1, stop = 4)) + 1,
-                  max = as.integer(substr(player_last_season, start = 1, stop = 4)) - 10,
-                  value = 1995
+                  min = as.integer(substr(player_first_season, start = 1, stop = 4)),
+                  max = as.integer(substr(player_last_season, start = 1, stop = 4)),
+                  value = as.integer(substr(player_first_season, start = 1, stop = 4))
                 )
               ),
-              mainPanel(plotOutput(outputId = "distplot"))
+              mainPanel(
+                plotOutput(outputId = "distplot")
+              )
+            ),
+            sidebarLayout(
+              sidebarPanel(
+                selectInput(inputId='var',
+                            label="Select the team",
+                            choices = player_teams,
+                            selected = 'wt')
+              ),
+              mainPanel(
+                plotlyOutput(outputId = 'plot_by_team')
+              )
             ),
             fluidRow(column(width = 2, "Team(s)")),
             fluidRow(column(width = 6, checkboxInput('change_sign','Team A'))),
@@ -113,90 +199,32 @@ ui <- fluidPage(
 # ---- Backend ----
 server <- function(input, output, session) {
   
-  # Add player name and player slug
-  player <- "Jason Kidd"
-  player_name_split <- tolower(strsplit(player, " ")[[1]])
-  slug_ln <- substr(player_name_split[2], start = 1, stop = 5)
-  slug_fn <- substr(player_name_split[1], start = 1, stop = 2)
-  slug <- paste0(slug_ln, slug_fn, '01')#"bryanko01" 
-  
-  # define player page URL and player image URL
-  url <- paste0("https://www.basketball-reference.com/players/",substr(slug,1,1),"/",slug,".html")
-  image_url <- paste0("https://www.basketball-reference.com/req/202106291/images/players/",slug,".jpg")
-  
-  # Read total stats
-  ttl_stat <- url %>%
-    read_html %>%
-    html_node("#totals") %>% 
-    html_table()
-  
-  # Read advanced stats
-  adv_stat <- url %>%
-    read_html %>%
-    html_node("#advanced") %>% 
-    html_table()
-  
-  # Merge stats tables
-  total_stats <- merge(ttl_stat, adv_stat, by=c("Season","Age", "Tm", "Lg", "Pos", "G", "MP"))
-  
-  # Prepare player stats table
-  player_stats <- total_stats |> select(c('Season', 'Age', 'Tm', 'Pos', 'G', 'FG%', 'TRB', 'AST', 'STL', 'BLK', 'PTS'))
-  
-  player_stats <- player_stats |>
-    mutate('TRB per game' = round(player_stats$TRB / player_stats$G, 2),
-           'AST per game' = round(player_stats$AST / player_stats$G, 2),
-           'STL per game' = round(player_stats$STL / player_stats$G, 2),
-           'BLK per game' = round(player_stats$BLK / player_stats$G, 2),
-           'PTS per game' = round(player_stats$PTS / player_stats$G, 2))
-  
-  # remove missing values
-  player_exp_no_na <- player_stats |> filter(!is.na(player_stats$Age))
-  
-  # Get PLayer Experience
-  player_exp <- length(unique(player_exp_no_na$Age))
-  
-  # Get Player career year range
-  player_first_season <- min(unique(player_exp_no_na$Season))
-  player_last_season <- max(unique(player_exp_no_na$Season))
-  
-  # Get PLayer Age
-  player_age <- max(player_exp_no_na$Age, na.rm = TRUE)
-  
-  # Get PLayer Positions
-  player_position_ls <- unique(player_exp_no_na$Pos)
-  
-  player_positions <- ''
-  
-  for (i in seq_along(player_position_ls)) {
-    player_positions <- paste0(player_positions, 
-                               position_map(player_position_ls[i]), 
-                               collapse = "")
-    if (i != length(player_position_ls)) {
-      player_positions <- paste0(player_positions, ' & ', collapse = "")
-    }
-  }
-  
-  # Get PLayer teams
-  player_team_ls <- unique(player_exp_no_na$Tm)
-  
-  player_teams <- c()
-  
-  for (team in player_team_ls) {
-    player_teams <- c(player_teams, nba_teams_map(team))
-  }
-  
   thematic::thematic_shiny()
+  
   output$distplot <- renderPlot({
     # generate bins
     x <- player_exp_no_na$`PTS per game`
-    bins <- seq(min(x), max(x), length.out = input$careeryearslider + 1)
+    bins <- seq(min(x), max(x), length.out = input$careeryearslider)
     
     # draw the histogram with the specified number of bins
     hist(x,
-         breaks = bins, col = "darkgray", border = "white",
+         breaks = bins, 
+         col = "darkgray", 
+         border = "white",
          xlab = "Career year/season",
          main = "Histogram of Points per game by season"
     )
+  })
+  
+  output$plot_by_team <- renderPlotly({
+    
+    
+    
+    ggplotly( 
+      ggplot(mtcars, 
+             aes(.data[[input$var]], mpg)) + # Read book chapter 12 to understand why you have to use .data
+        geom_point() +
+        geom_smooth())
   })
   
 }
