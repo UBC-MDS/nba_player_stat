76c97d05460e2d319fcee726839bcc22fff90bfd
Chen Lin <chen.lin.0404@gmail.com>
2023-03-14 23:32:36 -0700

    fix image render issue which no longer update image for all app instances

--- src/App.R
+++ src/App.R
@@ -192,7 +192,7 @@ player <- str_to_title('Vince Carter')
 
 player_info <- update_player(player)
 
-image_url <- reactiveVal(player_info$image_url)
+image_url <- player_info$image_url
 player_positions <- player_info$player_positions
 player_age <- player_info$player_age
 player_exp <- player_info$player_exp
@@ -224,6 +224,7 @@ ui <- fluidPage(
                       column(width=4, align="center",
                              # img(id="player_image", src=image_url, width=100),
                              uiOutput("player_image_ui")),
+                             # imageOutput("player_image_ui", width="150px")),
                       column(id = "player_intro", width = 8, align = "left",
                              fluidRow(column(width = 12,
                                              h2(
@@ -331,11 +332,23 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   output$player_image_ui <- renderUI({
     tags$img(
-      id = "player_image",
-      src = image_url(),
+      id = "player_image_",
+      src = image_url,
       width = 120
     )
   })
+  
+  # output$image <- renderImage({
+  #   list(src = image_url, width = "100%") # Set the width to 100% to make the image responsive
+  # }, deleteFile = TRUE)
+  
+  # output$player_image_ui <- renderImage({
+  #   filename <- normalizePath(image_url)
+  #   
+  #   # Return a list containing the filename and alt text
+  #   list(href = filename)
+  #   
+  # }, deleteFile = FALSE)
   
   output$player_full_name <- renderText({
     'Vince Carter'
@@ -526,7 +539,15 @@ server <- function(input, output, session) {
       
       url_status_200 <- TRUE
       
-      image_url(image_url)      
+      # image_url(image_url)  
+      
+      output$player_image_ui <- renderUI({
+        tags$img(
+          id = "player_image_",
+          src = image_url,
+          width = 120
+        )
+      })
       
       output$player_full_name <- renderText({
         player
