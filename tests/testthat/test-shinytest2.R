72fb81ea0ff94522aeb2a2f202c377e5bf5d3d89
Suraporn <suraporn83@gmail.com>
2023-03-17 15:44:10 -0700

    update test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -12,18 +12,16 @@ test_that("{shinytest2} recording: test_stat_assist_value", {
 
 
 
-test_that("{shinytest2} recording: test_stat_block_screenshort", {
-  app <- AppDriver$new(variant = platform_variant(), name = "test_stat_block_screenshort", 
-      height = 794, width = 1211)
-  app$set_inputs(stat_select = "Blocks")
-  app$expect_screenshot()
-})
-
-
-
 test_that("{shinytest2} recording: test_stat_rebound_value", {
   app <- AppDriver$new(name = "test_stat_rebound_value", height = 794, width = 1211)
   app$set_inputs(stat_select = "Rebounds")
   app$expect_values()
 })
 
+
+
+test_that("{shinytest2} recording: test_stat_block_value", {
+  app <- AppDriver$new(name = "test_stat_block_value", height = 866, width = 1211)
+  app$set_inputs(stat_select = "Blocks")
+  app$expect_values()
+})
